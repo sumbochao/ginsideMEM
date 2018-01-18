6f3256e706a62d8d74a4fd24ba44b59b91f9bd6d
phunld <phunld@mecorp.vn>
2018-01-18 14:42:18 +0700

    \application\core\v1\Enum

---
+++ application/core/v1/Enum/EmptyEnum.php
@@ -0,0 +1,35 @@
+<?php
+
+
+namespace Misc\Enum;
+
+class EmptyEnum extends AbstractEnum {
+
+  /**
+   * @return array
+   */
+  public function getArrayCopy() {
+    return array();
+  }
+
+  /**
+   * @return array
+   */
+  public function getNames() {
+    return array();
+  }
+
+  /**
+   * @return array
+   */
+  public function getValues() {
+    return array();
+  }
+
+  /**
+   * @return array
+   */
+  public function getValuesMap() {
+    return array();
+  }
+}
---
+++ application/core/v1/Enum/EnumInstanceInterface.php
@@ -0,0 +1,70 @@
+<?php
+
+
+namespace Misc\Enum;
+
+interface EnumInstanceInterface {
+
+  /**
+   * @return EnumInstanceInterface
+   */
+  public static function getInstance();
+
+  /**
+   * @return array
+   */
+  public function getArrayCopy();
+
+  /**
+   * @return array
+   */
+  public function getNames();
+
+  /**
+   * @return array
+   */
+  public function getValues();
+
+  /**
+   * @return array
+   */
+  public function getValuesMap();
+
+  /**
+   * @param string|int|float $name
+   * @return mixed
+   */
+  public function getValueForName($name);
+
+  /**
+   * @param string|int|float $name
+   * @return mixed
+   * @throws \InvalidArgumentException
+   */
+  public function assureValueForName($name);
+
+  /**
+   * @param string|int|float $name
+   * @return bool
+   */
+  public function isValid($name);
+
+  /**
+   * @param string|int|float $name
+   * @return void
+   * @throws \InvalidArgumentException
+   */
+  public function assureIsValid($name);
+
+  /**
+   * @param mixed $value
+   * @return bool
+   */
+  public function isValidValue($value);
+
+  /**
+   * @param mixed $value
+   * @throws \InvalidArgumentException
+   */
+  public function assureIsValidValue($value);
+}
